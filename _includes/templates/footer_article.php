5ef8586ba4f11fa74cc6d16669cce3c1628d38b6
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-02-21 21:50:01 -0500

    Initial commit

---
+++ _includes/templates/footer_article.php
@@ -0,0 +1,3 @@
+                </div>
+                <tabledesmatieres></tabledesmatieres>
+            </article>
---
+++ _includes/templates/footer_main.php
@@ -0,0 +1,4 @@
+        </main>
+        <footer>
+            <div id="copyright">© <?php echo lang('copyright'); ?><br><?php echo $PAGE->author; ?>, <?php echo date('Y'); ?></div>
+        </footer>
