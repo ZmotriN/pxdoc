4769fe364af4f87d36b82faf9178d0a8520d004a
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-08-18 19:01:50 -0400

    Global Try/Catch

--- _bin/scripts/pxdoc.php
+++ _bin/scripts/pxdoc.php
@@ -1,30 +1,36 @@
 <?php
-require_once(__DIR__ . '/utils.php');
 
-$PAGE = null;
+try {
+    
+    require_once(__DIR__ . '/utils.php');
 
-if (!isset($argv[1])) err("Invalid argument.");
-if (!$target = realpath($argv[1])) err("Invalid target.");
+    $PAGE = null;
 
-if (is_dir($target)) {
-    if (!$seed = PXPros::findSeed($target)) err("No project configuration found.");
-    $prj = new PXPros($seed);
-    foreach (dig($target . '/*.php') as $file) {
-        $parent = pathinfo(pathinfo($file, PATHINFO_DIRNAME), PATHINFO_BASENAME);
-        if (strpos($parent, '_') === 0) continue;
-        if (strpos(pathinfo($file, PATHINFO_FILENAME), '_') !== 0) continue;
+    if (!isset($argv[1])) err("Invalid argument.");
+    if (!$target = realpath($argv[1])) err("Invalid target.");
+
+    if (is_dir($target)) {
+        if (!$seed = PXPros::findSeed($target)) err("No project configuration found.");
+        $prj = new PXPros($seed);
+        foreach (dig($target . '/*.php') as $file) {
+            $parent = pathinfo(pathinfo($file, PATHINFO_DIRNAME), PATHINFO_BASENAME);
+            if (strpos($parent, '_') === 0) continue;
+            if (strpos(pathinfo($file, PATHINFO_FILENAME), '_') !== 0) continue;
+            echo 'Render: ';
+            echo str_replace([pathinfo($seed, PATHINFO_DIRNAME), S, '\\'], ['', '/', '/'], $file) . RN;
+            $prj->render($file);
+        }
+    } elseif (preg_match('#^_(.*)\.php$#i', pathinfo($target, PATHINFO_BASENAME), $m)) {
+        if (!$seed = PXPros::findSeed($target)) err("No project configuration found.");
+        $pxpros = new PXPros($seed);
         echo 'Render: ';
-        echo str_replace([pathinfo($seed, PATHINFO_DIRNAME), S, '\\'], ['', '/', '/'], $file) . RN;
-        $prj->render($file);
+        echo str_replace([pathinfo($seed, PATHINFO_DIRNAME), S, '\\'], ['', '/', '/'], $target) . RN;
+        $pxpros->render($target);
+    } else {
+        throw new Exception("Invalid target.");
     }
-} elseif (preg_match('#^_(.*)\.php$#i', pathinfo($target, PATHINFO_BASENAME), $m)) {
-    if (!$seed = PXPros::findSeed($target)) err("No project configuration found.");
-    $pxpros = new PXPros($seed);
-    echo 'Render: ';
-    echo str_replace([pathinfo($seed, PATHINFO_DIRNAME), S, '\\'], ['', '/', '/'], $target) . RN;
-    $pxpros->render($target);
-} else {
-    err("Invalid target.");
+} catch(Exception $e) {
+    err($e->getMessage());
 }
 
 exit(0);
