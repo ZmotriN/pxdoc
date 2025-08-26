f86c96f8eaca571339bc9779d0a876cb25a95cc0
Maxime Larrivée-Roy <zmotrin@gmail.com>
2025-08-26 18:14:33 -0400

    Refactor loadJsonProperties

--- jscripts/pxdoc.core.js
+++ jscripts/pxdoc.core.js
@@ -86,29 +86,32 @@ self.loadScript = async function(endpoint, params = {}, isAsync = false) {
 /******************************************************
  *           Load Json properties for target          *
  ******************************************************/
-self.loadJsonProperties = async function (target, files = []) {
-    const results = await Promise.allSettled(
-        files.map(async (url) => {
-            const res = await fetch(url);
-            const id = url.match(/([^\/]+)(?=\.\w+$)/)?.[0] ?? url;
-            let data = null;
-            try { data = await res.json(); } catch (_) { }
-            return { url, id, status: res.status, ok: res.ok, data };
-        })
-    );
-    for (const r of results) {
-        if (r.status === 'fulfilled') {
-            const { url, id, status, ok, data } = r.value;
-            if (!ok) {
-                console.error(`${id} [${status} - ERREUR] ${url}`);
-                continue;
-            }
-            target[id] = data;
-        } else {
-            console.error('Erreur réseau/JS pendant le chargement :', r.reason);
-        }
+self.loadJsonProperties = async function (target, files = {}) {
+  const entries = Object.entries(files); // [["sections","/data/sections.json"], ...]
+
+  const results = await Promise.allSettled(
+    entries.map(async ([key, url]) => {
+      const res = await fetch(url);
+      let data = null;
+      try { data = await res.json(); } catch (_) {}
+      return { key, url, status: res.status, ok: res.ok, data };
+    })
+  );
+
+  for (const r of results) {
+    if (r.status === 'fulfilled') {
+      const { key, url, status, ok, data } = r.value;
+      if (!ok) {
+        console.error(`${key} [${status} - ERREUR] ${url}`);
+        continue;
+      }
+      target[key] = data;
+    } else {
+      console.error('Erreur réseau/JS pendant le chargement :', r.reason);
     }
-    return target;
+  }
+
+  return target;
 };
 
 
